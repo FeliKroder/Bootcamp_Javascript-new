a594e6be34896747a13bd49e603d5b0f1d66da9a
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-08-10 11:20:18 +0200

    complete challenge

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,16 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  if (animals === "cats") {
+    return `I totally love cats!`;
+  } else if (animals) {
+    return `I like ${animals}!`;
+  } else animals === "";
+  return `I do not like animals at all!`;
+}
+
+console.clear();
+
+getAnimal("cats");
+console.log(getAnimal("cats"));
+console.log(getAnimal("dogs"));
+console.log(getAnimal("fish"));
+console.log(getAnimal());
